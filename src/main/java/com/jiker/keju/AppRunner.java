44d467a85f6a1148ab12913eff9eb8c741924792
Yao Kang 姚抗(IS <sja9yb@sgmwh.shanghaigm.com>
2020-02-28 18:44:14 +0800

    9-version

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class AppRunner {
 
-    public static void main(String  args[]) throws Exception {
+    public static void main(String  args[]){
         try { String receipt = "";
         ArgsRead argsRead = new ArgsRead(args[0]);
         List list = argsRead.getCMD();
