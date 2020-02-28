6f5f8978aef10209259710d50a17e0229b65e1f2
Yao Kang 姚抗(IS <sja9yb@sgmwh.shanghaigm.com>
2020-02-28 18:28:09 +0800

    7version

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class AppRunner {
 
     public static void main(String[] args) throws Exception {
-        String testDataFile = "input.txt";
+        String testDataFile = args[0];
         String receipt = "";
         ArgsRead argsRead = new ArgsRead(testDataFile);
         List list = argsRead.getCMD();
