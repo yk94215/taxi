2eefc905e27e1b1f73690842c0e88ec32796c659
Yao Kang 姚抗(IS <sja9yb@sgmwh.shanghaigm.com>
2020-02-28 16:33:58 +0800

    Initial commit

---
+++ src/test/ArgsReadTest.java
@@ -0,0 +1,20 @@
+package com.jiker.keju.test;
+import com.jiker.keju.Cmd;
+import com.jiker.keju.resources.ArgsRead;
+import org.junit.Test;
+import java.util.List;
+
+import static  org.junit.Assert.assertEquals;
+public class ArgsReadTest {
+    @Test
+    public void testGetCMD() throws Exception {
+        ArgsRead argsRead = new ArgsRead("testData.txt");
+        List list = argsRead.getCMD();
+        Cmd cmd= (Cmd) list.get(0);
+        assertEquals(cmd.getDistance(),123);
+        assertEquals(cmd.getWating(),0);
+        Cmd cmd2= (Cmd) list.get(9);
+        assertEquals(cmd2.getDistance(),123);
+        assertEquals(cmd2.getWating(),10);
+    }
+}
---
+++ src/test/ExcuteCmdTest.java
@@ -0,0 +1,22 @@
+package com.jiker.keju;
+import com.jiker.keju.ExCuteCmd;
+import com.jiker.keju.resources.ArgsRead;
+import org.junit.Test;
+import java.util.List;
+import static  org.junit.Assert.assertEquals;
+public class ExcuteCmdTest
+{
+@Test
+    public void testExcute() throws Exception {
+    ArgsRead argsRead = new ArgsRead("testData.txt");
+    List list = argsRead.getCMD();
+    ExCuteCmd exCuteCmd = new ExCuteCmd();
+    String ret = exCuteCmd.excute(list);
+    assertEquals( exCuteCmd.excute(list),"收费6元\n" +
+            "收费7元\n" +
+            "收费13元\n" +
+            "收费7元\n");
+    System.out.println(ret);
+
+}
+}
